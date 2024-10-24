3ca1903cefe136f3088f3234f595f4b00fe714ab
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-24 22:22:03 +0100

    Following a circuit

--- Assets/FollowWaypoint.cs
+++ Assets/FollowWaypoint.cs
@@ -10,21 +10,34 @@ public class FollowWaypoint : MonoBehaviour
     public float speed = 10;
     public float rotSpeed = 10;
 
+    GameObject tracker;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        Destroy(tracker.GetComponent<Collider>());
+        tracker.transform.position = transform.position;
+        tracker.transform.rotation = transform.rotation;
+    }
+
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, wayPoints[currentWaypoint].transform.position) < 3)
+            currentWaypoint++;
+
+
+        if (currentWaypoint >= wayPoints.Length)
+            currentWaypoint = 0;
+
+        tracker.transform.LookAt(wayPoints[currentWaypoint].transform);
+        tracker.transform.Translate(0, 0, 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.transform.position, wayPoints[currentWaypoint].transform.position) < 3)
-            currentWaypoint++;
-        
-
-        if (currentWaypoint >= wayPoints.Length)
-            currentWaypoint = 0;
+        ProgressTracker();
 
         //transform.LookAt(wayPoints[currentWaypoint].transform);
         transform.Translate(0, 0, speed * Time.deltaTime);
