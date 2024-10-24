9fa3b17053130b77b8b444c33b73c229101b3f2f
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-24 21:31:14 +0100

    Initial commit

---
+++ Assets/FollowWaypoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWaypoint : MonoBehaviour
+{
+    public GameObject[] wayPoints;
+    int currentWaypoint = 0;
+
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(this.transform.position, wayPoints[currentWaypoint].transform.position) < 3)
+            currentWaypoint++;
+        
+
+        if (currentWaypoint >= wayPoints.Length)
+            currentWaypoint = 0;
+
+        transform.LookAt(wayPoints[currentWaypoint].transform);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
